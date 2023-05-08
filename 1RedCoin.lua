52438de91f636913e76fa52e96b1e926d9eecd88
TheDragonary <112120245+TheDragonary@users.noreply.github.com>
2023-05-08 17:19:14 +0100

    Add files via upload

---
+++ 1RedCoin.lua
@@ -0,0 +1,12 @@
+-- name: 1 Red Coin
+-- description: You only need 1 red coin to get the star\n\nMod created by Dragonary
+
+---@param obj Object
+function bhv_custom_red_coin_star_loop(obj)
+    if obj.oHiddenStarTriggerCounter >= gMarioStates[0].area.numRedCoins - 7 then
+        obj.oAction = 1
+    end
+end
+
+hook_behavior(id_bhvHiddenRedCoinStar, OBJ_LIST_LEVEL, false, nil, bhv_custom_red_coin_star_loop)
+hook_behavior(id_bhvBowserCourseRedCoinStar, OBJ_LIST_LEVEL, false, nil, bhv_custom_red_coin_star_loop)
